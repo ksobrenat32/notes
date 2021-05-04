59e0548072602ae614da9c12fa3be718f62c8aea
ksobrenat32 <privateaccounts32@tutanota.com>
2021-05-04 12:58:26 -0500

    system config instead of user

--- containers/podman-registries.sh
+++ containers/podman-registries.sh
@@ -2,6 +2,4 @@
 
 echo "This script adds the docker registrie to podman configuration"
 
-mkdir $HOME/.config/containers
-echo '[registries.search]' >> $HOME/.config/containers/registries.conf
-echo 'registries = ['docker.io']' >> $HOME/.config/containers/registries.conf
+echo -e "[registries.search]\nregistries = ['docker.io']" | sudo tee /etc/containers/registries.conf
