873dbeb1359dd27917ccb2be202556019b102b75
ksobrenat32 <privateaccounts32@tutanota.com>
2021-06-10 10:19:32 -0500

    script

--- containers/ping-rootless.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo 'This script allows rootless podman containers to use ping, if you have more than one user you should change the last value with the last gid your user can use, you can locate the number in /etc/subgid '
-
-sudo echo 'net.ipv4.ping_group_range=0 165535' >> /etc/sysctl.d/podman-ping.conf
--- containers/podman-registries.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-echo "This script adds the docker registrie to podman configuration"
-
-echo -e "[registries.search]\nregistries = ['docker.io']" | sudo tee /etc/containers/registries.conf
